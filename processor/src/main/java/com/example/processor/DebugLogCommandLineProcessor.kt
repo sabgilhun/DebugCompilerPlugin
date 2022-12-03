74d7dd381308ee69a5c967b5df01af7bdb8698e7
sabgilhun <sabgilhun@gmail.com>
2022-12-03 19:23:39 +0900

    CommandLineProcessor 설정 추가

---
+++ processor/src/main/java/com/example/processor/DebugLogCommandLineProcessor.kt
@@ -0,0 +1,37 @@
+package com.example.processor
+
+import com.google.auto.service.AutoService
+import org.jetbrains.kotlin.compiler.plugin.AbstractCliOption
+import org.jetbrains.kotlin.compiler.plugin.CliOption
+import org.jetbrains.kotlin.compiler.plugin.CommandLineProcessor
+import org.jetbrains.kotlin.config.CompilerConfiguration
+import org.jetbrains.kotlin.config.CompilerConfigurationKey
+
+@AutoService(CommandLineProcessor::class)
+class DebugLogCommandLineProcessor : CommandLineProcessor {
+
+    override val pluginId = "debuglog-compiler-plugin"
+
+    override val pluginOptions: Collection<AbstractCliOption> = listOf(
+        CliOption(
+            optionName = "useTimeElapsedPrinting",
+            valueDescription = "Boolean",
+            description = "option for using time elapsed print feature",
+            required = true,
+        ),
+    )
+
+    override fun processOption(
+        option: AbstractCliOption,
+        value: String,
+        configuration: CompilerConfiguration,
+    ) {
+        if (option.optionName == "useTimeElapsedPrinting") {
+            configuration.put(argName, value.toBoolean())
+        }
+    }
+
+    companion object {
+        val argName = CompilerConfigurationKey<Boolean>("useTimeElapsedPrinting")
+    }
+}
