b1ff586af2145911af0e58ab4e6fd0b92c7c077b
sabgilhun <sabgilhun@gmail.com>
2022-12-03 21:52:55 +0900

    annotation 추가 및 android 모듈에서 사용

--- app/src/main/java/com/example/debuglogcompileplugin/MainActivity.kt
+++ app/src/main/java/com/example/debuglogcompileplugin/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.example.debuglogcompileplugin
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import com.example.annotation.DebugLog
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<View>(R.id.button).setOnClickListener {
+            annotationTest("hello")
+        }
+    }
+
+    @DebugLog
+    private fun annotationTest(param: String, paramHavingDefaultValue: String = "world") {
+        println("$param $paramHavingDefaultValue")
     }
 }
